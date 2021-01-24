703b891f47c63bc855a21045474af39d0d0624f5
millionfood <75560997+millionfood@users.noreply.github.com>
2021-01-24 18:24:37 +0900

    todolist1.1

--- bg.js
+++ bg.js
@@ -6,7 +6,7 @@ const IMG_NUMBER = 3;
 
 function paintImage(imgNumber){
     const image = new Image();
-    image.src = `../img/${imgNumber + 1}.jpg`;
+    image.src = `img/${imgNumber + 1}.jpg`;
     image.classList.add('bgImage');
     body.prepend(image);
     
